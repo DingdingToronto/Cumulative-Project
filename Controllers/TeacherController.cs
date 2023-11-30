2a7be595604e7795a1a27bf09e7be9fa1145d25b
DingdingToronto <jiabaoding6@gmail.com>
2023-11-30 18:11:58 -0500

    part 2

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -37,24 +37,39 @@ namespace SchoolProject.Controllers
 
         //POST : /Teacher/Create
         [HttpPost]
-        public ActionResult Create(string TeacherFname = null, string TeacherLname = null, double TeacherSalary = 0.00, DateTime? TeacherDate = null, string TeacherNumber = null)
+        
+        public ActionResult Create(string TeacherFname, string TeacherLname, double TeacherSalary, DateTime TeacherDate, string TeacherNumber)
         {
-            Teacher NewTeacher = new Teacher();
-            NewTeacher.TeacherFname = TeacherFname;
-            NewTeacher.TeacherLname = TeacherLname;
-            NewTeacher.TeacherSalary = TeacherSalary;
-            if (TeacherDate.HasValue)
+            // Server-side validation
+            if (string.IsNullOrWhiteSpace(TeacherFname) ||
+                string.IsNullOrWhiteSpace(TeacherLname) ||
+                string.IsNullOrWhiteSpace(TeacherNumber) ||
+                TeacherSalary <= 0 ||
+                TeacherDate == DateTime.MinValue)
             {
-                NewTeacher.TeacherDate = TeacherDate.Value;
+                // Invalid data, return to the create view with an error message
+                TempData["ErrorMessage"] = "Please fill all";
+                return RedirectToAction("Create");
             }
 
-            NewTeacher.TeacherNumber = TeacherNumber;
+            // Data is valid, create a new Teacher instance
+            Teacher NewTeacher = new Teacher
+            {
+                TeacherFname = TeacherFname,
+                TeacherLname = TeacherLname,
+                TeacherSalary = TeacherSalary,
+                TeacherDate = TeacherDate,
+                TeacherNumber = TeacherNumber
+            };
 
+            // Call the AddTeacher method in your TeacherDataController to add the teacher to the database
             TeacherDataController controller = new TeacherDataController();
             controller.AddTeacher(NewTeacher);
 
+            // Redirect to the list of teachers or any other appropriate action
             return RedirectToAction("List");
         }
+
 
         //GET : /Teacher/New
         public ActionResult New()
